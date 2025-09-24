f69c887771629bdca133dd741f84e455ab002e28
MrYapikZ <yafialhaidar18@gmail.com>
2025-09-24 16:15:39 +0700

    release: v0.1.0

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class MainUI(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle("Relocate Path Win2Linux")
-        self.ui.label_version.setText("v0.0.0")
+        self.ui.label_version.setText("v0.1.0")
 
         self.ui.tabWidget_main.addTab(RelocateHandler(), "Relocate")
 
